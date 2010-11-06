58bf8840ab404edb820bc1c53d489391f8df5d4d
Matt Mueller <muellermr@gmail.com>
2010-11-06 08:51:57 -0500

    Initial twitter status methods

--- Gemfile
+++ Gemfile
@@ -6,4 +6,4 @@ gem 'mysql', '2.8.1'
 gem "compass", ">= 0.10.6"
 gem "haml", ">= 3.0.23"
 gem "twitter", "= 1.0.0.rc.6"
-
+gem "oauth", ">= 0.4.4"
--- app/models/twitter_status.rb
+++ app/models/twitter_status.rb
@@ -1,3 +1,11 @@
 class TwitterStatus < Activity
-  
+
+  def self.authorize_access
+    Twitter::Client.new("hiding credentials")
+  end
+
+  def self.get_historic_statuses
+    TwitterStatus.authorize_access.user_timeline('matt_mueller', :count => 100)
+  end
+
 end
