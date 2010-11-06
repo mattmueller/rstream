c8a4deaf0098a3f4d1f5f4cf7e495db9750ed0cc
Matt Mueller <muellermr@gmail.com>
2010-11-06 07:23:53 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tmmnew::Application.config.secret_token = '0d31b1963612b4dceb4a559238c06a6e8dbbc2828d832f8d8a6170260de6ac2ab4a37eefe60d30d39948e28d6b79ae58d38bfcfbbcc60ece04c190bee96f0fdc'
