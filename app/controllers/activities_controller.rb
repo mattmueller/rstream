a0e4ffc4139ea5ab4e45d74a8912aafaa2a1fc3d
Matt Mueller <muellermr@gmail.com>
2010-11-09 17:41:15 -0600

    Some initial styling of twitter activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,8 @@
 class ActivitiesController < ApplicationController
   
   def index
-    
+    @current_tab = 'stream'
+    @activities = TwitterStatus.all(:order => 'created_at desc', :limit => 20)
   end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_sidebar_totals
+
+  def set_sidebar_totals
+    @stream_count = Activity.count
+    @twitter_count = TwitterStatus.count
+    @foursquare_count = FoursquareCheckin.count
+    @google_reader_count = GoogleReaderShare.count
+    @flickr_count = FlickrPhoto.count
+  end
 end
