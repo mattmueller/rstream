c3f6b5508970052cf03c5eaa4b237e00270fc279
Matt Mueller <muellermr@gmail.com>
2010-11-10 07:03:00 -0600

    Adding delicious activities

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,7 +2,7 @@ class ActivitiesController < ApplicationController
   
   def index
     @current_tab = 'stream'
-    @activities = Activity.all(:order => 'created_at desc', :limit => 20)
+    @activities = DeliciousBookmark.all(:order => 'created_at desc', :limit => 20)
   end
 
 end
