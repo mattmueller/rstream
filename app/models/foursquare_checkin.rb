bbaa5740d220d4ea6977944348cfad1a5f2d5ee3
Matt Mueller <muellermr@gmail.com>
2010-11-10 06:29:24 -0600

    Adding google reader share and modifying foursquare date parsing

--- app/models/foursquare_checkin.rb
+++ app/models/foursquare_checkin.rb
@@ -42,7 +42,7 @@ class FoursquareCheckin < Activity
   end
 
   def self.log_activity(checkin)
-    FoursquareCheckin.create(:external_id => checkin['id'], :created_at => checkin.created.to_time.utc, :title => checkin['display'], :content => checkin.shout, :metadata => { "venue_name" => checkin.venue.name, "venue_id" => checkin.venue['id'], "lat" => checkin.venue.geolat, "lon" => checkin.venue.geolong })
+    FoursquareCheckin.create(:external_id => checkin['id'], :created_at => Time.parse(checkin.created).utc, :title => checkin['display'], :content => checkin.shout, :metadata => { "venue_name" => checkin.venue.name, "venue_id" => checkin.venue['id'], "lat" => checkin.venue.geolat, "lon" => checkin.venue.geolong })
   end
 
 end
