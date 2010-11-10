df7cf37dcdb86d2f4397e9c0dcd78f31d570d2d8
Matt Mueller <muellermr@gmail.com>
2010-11-10 06:42:40 -0600

    Adding flickr photo styling

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,7 +2,7 @@ class ActivitiesController < ApplicationController
   
   def index
     @current_tab = 'stream'
-    @activities = FoursquareCheckin.all(:order => 'created_at desc', :limit => 20)
+    @activities = Activity.all(:order => 'created_at desc', :limit => 20)
   end
 
 end
