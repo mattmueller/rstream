b285f1a46fac515ed1aeaee3d81542d40f278e2b
Matt Mueller <muellermr@gmail.com>
2010-11-13 06:18:46 -0600

    Constants now define what is shown in sidebar as well as links on activity items.
    Pulling sidebar into a partial

---
+++ config/initializers/constants.rb
@@ -0,0 +1,10 @@
+#These constants control two things:
+#  1. What nav links are shown in the sidebar
+#  2. The link used for the activity type icon at the top left of every activity.
+
+DELICIOUS_USERNAME = ""
+FLICKR_USERNAME = ""
+FOURSQUARE_ID = ""
+GOOGLE_READER_ID = ""
+TWITTER_USERNAME = ""
+
